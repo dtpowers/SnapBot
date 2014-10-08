54df3fd68638416b6f81435ac708a807e9b89137
Donovan Powers <dontpowers@hotmail.com>
2014-10-08 14:53:38 -0400

    changed while loop to not recreate objects each itteration

--- src/com/habosa/javasnap/Main.java
+++ src/com/habosa/javasnap/Main.java
@@ -10,9 +10,10 @@ public class Main {
 	//test
 
 	public static void main(String[] args) throws Exception {
-		while(true){
+		
 		FileMaitenence testCleaner = new FileMaitenence("snaps//", 1);
 		SnapBot test = new SnapBot("snapcmu", "dsnapback9472");
+		while(true){
 		testCleaner.cleanupFolder();
 		test.run();
 	    Thread.sleep(1000 * 60 * 1);
