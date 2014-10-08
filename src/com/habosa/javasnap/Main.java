a90a83cece3c04d4abaddc65d00b9339e006ddf8
Donovan Powers <dontpowers@hotmail.com>
2014-10-08 15:07:57 -0400

    draw username and pass from text files now

--- src/com/habosa/javasnap/Main.java
+++ src/com/habosa/javasnap/Main.java
@@ -3,23 +3,51 @@ package com.habosa.javasnap;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 public class Main {
-	//test
+	// test
 
 	public static void main(String[] args) throws Exception {
-		
+		String user = pullUser();
+		String pass = pullPass();
 		FileMaitenence testCleaner = new FileMaitenence("snaps//", 1);
-		SnapBot test = new SnapBot("snapcmu", "dsnapback9472");
-		while(true){
-		testCleaner.cleanupFolder();
-		test.run();
-	    Thread.sleep(1000 * 60 * 1);
+		SnapBot test = new SnapBot(user, pass);
+		while (true) {
+			testCleaner.cleanupFolder();
+			test.run();
+			Thread.sleep(1000 * 60 * 1);
 		}
-		
+
 	}
 
-	
+	public static String pullPass() throws IOException {
+		File pass = new File("password.txt");
+		String password = deserializeString(pass);
+		return password;
+	}
+
+	public static String pullUser() throws IOException{
+		File username = new File("username.txt");
+		String user = deserializeString(username);
+		return user;
+	}
+
+	public static String deserializeString(File file) throws IOException {
+		int len;
+		char[] chr = new char[4096];
+		final StringBuffer buffer = new StringBuffer();
+		final FileReader reader = new FileReader(file);
+		try {
+			while ((len = reader.read(chr)) > 0) {
+				buffer.append(chr, 0, len);
+			}
+		} finally {
+			reader.close();
+		}
+		return buffer.toString();
+	}
+
 }
